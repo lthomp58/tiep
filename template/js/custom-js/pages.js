2c74735aa2b9a0fc39162ef2df85927a87fdeef3
eduvlemes <edu.vlemes@gmail.com>
2020-11-10 02:37:44 -0300

    first commit with home n product adjusts

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,8 @@
 // Add your custom JavaScript for storefront pages here.
+if($('.page--home').length){
+    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2) > .banner').appendTo('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)');
+    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').remove();
+    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)').toggleClass('col-lg-4 col-lg-auto');
+    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').toggleClass('col-lg-4 col-lg-auto');
+
+}
