5895d7a67a8ab482e1d825700a72db6121c80662
Leonardo Matos <leomp120894@gmail.com>
2020-11-10 15:35:45 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/TheProduct.html': path.resolve(__dirname, '/template/js/custom-js/html/TheProduct.html')
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
     }
   }
 })
