94a4d80f737d01378457fed9f42e3cbb6f093680
Leonardo Matos <leomp120894@gmail.com>
2020-11-10 12:04:18 -0300

    Create storefront.webpack.js

---
+++ storefront.webpack.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/TheProduct.html': path.resolve(__dirname, '/template/js/custom-js/html/TheProduct.html')
+    }
+  }
+})
