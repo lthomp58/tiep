4a010987d73e3995385af1f8443aaf58520bb260
eduvlemes <edu.vlemes@gmail.com>
2020-11-13 15:22:12 -0300

    Página de Kit, Ajuste dos botões, Página de produto

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,6 +1,17 @@
 // Add your custom JavaScript for storefront pages here.
+/*!
+ * Simple jQuery Equal Heights
+ *
+ * Copyright (c) 2013 Matt Banks
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://docs.jquery.com/License
+ *
+ * @version 1.5.1
+ */
+!function(a){a.fn.equalHeights=function(){var b=0,c=a(this);return c.each(function(){var c=a(this).innerHeight();c>b&&(b=c)}),c.css("height",b)},a("[data-equal]").each(function(){var b=a(this),c=b.data("equal");b.find(c).equalHeights()})}(jQuery);
+
 if($('.page--home').length){
-    
     //mosaico principal
     $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2) > .banner').appendTo('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)');
     $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').remove();
@@ -15,6 +26,10 @@ if($('.page--home').length){
         let title = $(this).find('img').attr('alt');
         $(this).append('<label class="floating-title">'+ title +'</label>');
     });
+}
 
-
+if($('.product-card').length){
+    $(document).ready(function(){
+        $('#alpix .product-card__name').equalHeights();
+    });
 }
