c33a587a433a698ca3c00703161702ebe6934817
eduvlemes <edu.vlemes@gmail.com>
2020-11-17 23:21:15 -0300

    Ajustes mobile

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,15 +1,20 @@
 // Add your custom JavaScript for storefront pages here.
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
 !function(a){a.fn.equalHeights=function(){var b=0,c=a(this);return c.each(function(){var c=a(this).innerHeight();c>b&&(b=c)}),c.css("height",b)},a("[data-equal]").each(function(){var b=a(this),c=b.data("equal");b.find(c).equalHeights()})}(jQuery);
 $(document).ready(function(){
     $('body').addClass('loaded');
 })
 if($('.page--home').length){
     //mosaico principal
-    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2) > .banner').appendTo('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)');
-    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').remove();
-    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)').toggleClass('col-lg-4 col-lg-auto');
-    $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').toggleClass('col-lg-4 col-lg-auto');
-
+    if(isMobile){
+        
+    }else{
+        $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2) > .banner').appendTo('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)');
+        $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').remove();
+        $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(1)').toggleClass('col-lg-4 col-lg-auto');
+        $('#alpix .page--home > .sections > .banners-grid:first-child .col-12:nth-child(2)').toggleClass('col-lg-4 col-lg-auto');
+        
+    }
     //blocos categorias
     $('#alpix .page--home > .sections > .banners-grid:not(:first-child) .col-12').attr('class','col-12 col-md-auto px-1 py-1');
     $('#alpix .page--home > .sections > .banners-grid:not(:first-child) .row').addClass('justify-content-center align-items-center mx-0');
@@ -35,8 +40,7 @@ $(document).ready(function(){
         $('#alpix .product-card__name, #alpix .product_list_adjust').equalHeights();
     }
     $('#alpix .header__search-input').keyup(function(){
-        console.log('hi');
-        $('body .search__input').val($(this).val()).keydown().keyup().change();
+        $('body .search__input').val($(this).val()).[0].dispatchEvent(new Event('input'));
     });
 
     $('body').click(function(e){
